4d0705a15965b4896731848c7e90433d1817c16c
qmzo553 <qmzo553@gmail.com>
2023-02-27 20:11:46 +0900

    8

---
+++ HFDesignPattern/src/Encapsulation/Coffee.java
@@ -0,0 +1,28 @@
+package Encapsulation;
+
+public class Coffee {
+	
+	void prepareRecipe() {
+		boilWater();
+		brewCoffeeGrinds();
+		pourInCup();
+		addSugarAndMilk();
+		
+	}
+	
+	public void boilWater() {
+		System.out.println("물 끓이는 중");
+	}
+	
+	public void brewCoffeeGrinds() {
+		System.out.println("필터로 커피를 우려내는 중");
+	}
+	
+	public void pourInCup() {
+		System.out.println("컵에 따르는 중입니다.");
+	}
+	
+	public void addSugarAndMilk() {
+		System.out.println();
+	}
+}
