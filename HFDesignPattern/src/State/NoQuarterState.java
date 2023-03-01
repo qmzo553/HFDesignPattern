c474cfdabb5a09d9d8b911157ede66e2b9bd92b8
qmzo553 <qmzo553@gmail.com>
2023-03-01 12:07:45 +0900

    10

---
+++ HFDesignPattern/src/State/NoQuarterState.java
@@ -0,0 +1,32 @@
+package State;
+
+public class NoQuarterState implements State {
+	GumballMachine gumballMachine;
+	
+	public NoQuarterState(GumballMachine gumballMachine) {
+		this.gumballMachine = gumballMachine;
+	}
+	
+	public void insertQuarter() {
+		System.out.println("동전을 넣으셨습니다.");
+		gumballMachine.setState(gumballMachine.getHasQuarterState());
+	}
+	
+	public void ejectQuarter() {
+		System.out.println("동전을 넣어 주세요.");
+	}
+	
+	public void turnCrank() {
+		System.out.println("동전을 넣어 주세요.");
+	}
+	
+	public void dispense() {
+		System.out.println("동전을 넣어 주세요.");
+	}
+	
+	public void refill() { }
+	 
+	public String toString() {
+		return "waiting for quarter";
+	}
+}
---
+++ HFDesignPattern/src/State/SoldState.java
@@ -0,0 +1,37 @@
+package State;
+
+public class SoldState implements State {
+	GumballMachine gumballMachine;
+	 
+    public SoldState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+       
+	public void insertQuarter() {
+		System.out.println("알맹이를 내보내고 있습니다.");
+	}
+ 
+	public void ejectQuarter() {
+		System.out.println("이미 알맹이를 뽑으셨습니다.");
+	}
+ 
+	public void turnCrank() {
+		System.out.println("손잡이는 한번만 돌려주세요.");
+	}
+ 
+	public void dispense() {
+		gumballMachine.releaseBall();
+		if (gumballMachine.getCount() > 0) {
+			gumballMachine.setState(gumballMachine.getNoQuarterState());
+		} else {
+			System.out.println("Oops, out of gumballs!");
+			gumballMachine.setState(gumballMachine.getSoldOutState());
+		}
+	}
+	
+	public void refill() { }
+ 
+	public String toString() {
+		return "dispensing a gumball";
+	}
+}
